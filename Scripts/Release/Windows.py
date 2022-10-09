fb12592c0e38f82ab1a19cdbaca126b7d8683f34
Vladyslav Voinov <32337080+vvvar@users.noreply.github.com>
2022-10-09 18:03:34 +0200

    #16: Automate building of MSI Installed for Windows(Local + CI) (#37)
    * Add basic Windows installer build scripts;
    
    * Install python to win action
    
    * Fix path on Windows
    
    * Enable verbose logging;
    
    * Fix path to issc config
    
    * Fix path delimeter on win
    
    * Quote issc config according to docs;
    
    * Try new check_output command
    
    * Leave only compile issc
    
    * Enable issc output;
    
    * Attempt create MSI installer with WiX
    
    * Exclude .net
    
    * Simplify command exex
    
    * Add out path
    
    * Package vst3 file
    
    * Add missing cab media option
    
    * Cleanup scripts and adjust config
    
    * Fix iss code leftovers
    
    * Store obj file in same dir as wxs
    
    * Try simple msi structure;
    
    * Fix xml syntax errors;
    
    * Turn off MSI installer UI;
    
    * Explicitly use 64 and per-machine installations of MSI;
    
    * Configure VS Code for windows local build;
    
    * Update windows in github action; Customise UI for MSI installer;
    
    * Update MSI vertical banner;

--- Scripts/Release/Windows.py
+++ Scripts/Release/Windows.py
@@ -1,14 +1,20 @@
 """
-Create DMG Image(for MacOS) that contains release artifacts.
+Create executable(for Windows) that installs VST3.
 @note Python v3.8+ is required for this to run.
 """
 from ctypes import util
 import utils
 import argparse
+from pathlib import Path
+# WiX Default Configuration
+WIX_COMPILER_DEFAULT_PATH = "C:\Program Files (x86)\\WiX Toolset v3.11\\bin\\candle.exe"
+WIX_LINKER_DEFAULT_PATH = "C:\Program Files (x86)\\WiX Toolset v3.11\\bin\\light.exe"
 # Parse input arguments
 argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument('--force_upgrade_dependencies', type=bool,
-                             default=False, help='Force update global env dependencies live xcode, brew, node, etc')
+argument_parser.add_argument(
+    '--wix_compiler_path', type=str, default=WIX_COMPILER_DEFAULT_PATH, help='Path to WiX Compiler(usually called candle.exe)')
+argument_parser.add_argument(
+    '--wix_linker_path', type=str, default=WIX_LINKER_DEFAULT_PATH, help='Path to WiX Linker(usually called light.exe)')
 argument_parser.add_argument('--release_type', type=utils.ReleaseType, default=utils.ReleaseType.release,
                              choices=list(utils.ReleaseType), help='Type of release package, e.g. Release or Debug')
 argument_parser.add_argument('--preserve_tmp', type=bool, default=False,
@@ -16,16 +22,6 @@ argument_parser.add_argument('--preserve_tmp', type=bool, default=False,
 args = argument_parser.parse_args()
 # Print arguments
 utils.logVerbose("Arguments: ", args)
-# Conditionally, force install env dependencies
-if (args.force_upgrade_dependencies):
-    utils.logInfo("Force upgrading dependencies...")
-    utils.execCommand("softwareupdate --all --install --force")
-    utils.execCommand("brew update")
-    utils.execCommand("brew upgrade")
-    utils.execCommand("brew install node")
-# Ensure appdmg installed
-utils.logInfo("Installing appdmg...")
-utils.execCommand("npm install -g appdmg")
 # Create tmp dir
 utils.logInfo("Ensuring tmp and release dirs are clean and esists...")
 TMP_DIR_PATH = utils.getTmpDirPath()
@@ -33,23 +29,25 @@ utils.ensureDirEmpty(TMP_DIR_PATH)
 RELEASE_DIR_PATH = utils.getReleaseDirPath()
 utils.ensureDirEmpty(RELEASE_DIR_PATH)
 # Copy VST3, AU, configs and assets into it
-utils.logInfo("Copying VST3, AU, release assets and configs to tmo dir...")
+utils.logInfo("Copying VST3, release assets and configs to tmp dir...")
 utils.copyDirContentRecursive(
     utils.getBuildVST3DirPath(args.release_type), TMP_DIR_PATH)
-utils.copyDirContentRecursive(
-    utils.getBuildAUDirPath(args.release_type), TMP_DIR_PATH)
 utils.copyDirContentRecursive(
     utils.getProjectReleaseAssetsDirPath(), TMP_DIR_PATH)
 utils.copyDirContentRecursive(
     utils.getProjectReleaseConfigsDirPath(), TMP_DIR_PATH)
-# Create DMG
-utils.logInfo("Creating release image...")
-appDmgConfigFilePath = str(TMP_DIR_PATH) + "/appdmg-config.json"
-outputDmgFilePath = str(RELEASE_DIR_PATH) + "/PeakEater.dmg"
-utils.execCommand("appdmg " + appDmgConfigFilePath + " " + outputDmgFilePath)
+# Create installer
+utils.logInfo("Creating installer...")
+wixCompilerFullPath = utils.createPath(args.wix_compiler_path)
+wixLinkerFullPath = utils.createPath(args.wix_linker_path)
+wixConfigFullPath = TMP_DIR_PATH.joinpath("wix-config.wxs").resolve()
+wixObjectFullPath = TMP_DIR_PATH.joinpath("wix-config.wixobj").resolve()
+msiBuildFullPath = RELEASE_DIR_PATH.joinpath("PeakEater.msi").resolve()
+utils.execCommand(
+    f'""{str(wixCompilerFullPath)}" "{str(wixConfigFullPath)}" -o "{str(wixObjectFullPath)}" -arch "x64""')
+utils.execCommand(
+    f'""{str(wixLinkerFullPath)}" "{str(wixObjectFullPath)}" -o "{str(msiBuildFullPath)}" -ext "WixUIExtension""')
 # Conditionally, cleanup tmp
 if not args.preserve_tmp:
     utils.logInfo("Cleaning up tmp dir...")
     utils.rmDir(TMP_DIR_PATH)
-# Done, print where DMG is located
-utils.logInfo("Done! Dmg file may be found at: " + outputDmgFilePath)
