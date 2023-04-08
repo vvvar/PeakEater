9b7c3c21ef9655d2d6c0d18b217f4819c5a1be13
vvvar <voinovvladv@gmail.com>
2023-04-08 15:22:23 +0200

    Experiment: Test after signing

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class PeakEater(ConanFile):
 
     def generate(self):
         toolchain = CMakeToolchain(self)
-        toolchain.blocks.remove("apple_system") # Because Conan forces x86_64 build(from settings)
+        toolchain.blocks.remove("apple_system")  # Because Conan forces x86_64 build(from settings)
         toolchain.cache_variables["CONAN_PROJECT_NAME"] = str(self.name)
         toolchain.cache_variables["CONAN_PROJECT_COMPANY"] = str(self.company)
         toolchain.cache_variables["CONAN_PROJECT_VERSION"] = str(self.version)
@@ -65,8 +65,8 @@ class PeakEater(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        self._test()
         self._sign()
+        self._test()
         self._bundle()
 
     @log_conan_stage
