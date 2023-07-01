116a07b4bc80e98bcbab99ff1d3033ad8a720a63
vvvar <voinovvladv@gmail.com>
2023-07-01 19:31:02 +0200

    Fix incorrect bundling of CLAP and LV2 on macOS - they shall not be a CFBundle

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.env import VirtualBuildEnv
 
 class PeakEater(ConanFile):
     name = "peakeater"
-    version = "0.7.0"
+    version = "0.7.1"
     user = "vvvar"
     channel = "testing"
     company = "T-Audio"
